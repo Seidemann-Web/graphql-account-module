5ac7af8e1637d631f74cb5c7965479bad26b0f4f
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-06-10 11:09:50 +0200

    OXDEV-3521 Add product relation to rating data type

---
+++ src/Rating/DataType/RatingRelationService.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\Account\Rating\DataType;
+
+use OxidEsales\GraphQL\Catalogue\DataType\Product;
+use OxidEsales\GraphQL\Catalogue\Service\Product as ProductService;
+use TheCodingMachine\GraphQLite\Annotations\ExtendType;
+use TheCodingMachine\GraphQLite\Annotations\Field;
+
+/**
+ * @ExtendType(class=Rating::class)
+ */
+class RatingRelationService
+{
+    /** @var ProductService */
+    private $productService;
+
+    public function __construct(
+        ProductService $productService
+    ) {
+        $this->productService = $productService;
+    }
+
+    /**
+     * @Field()
+     */
+    public function product(Rating $rating): ?Product
+    {
+        $ratingModel = $rating->getEshopModel();
+
+        if ($ratingModel->getFieldData('oxtype') !== 'oxarticle') {
+            return null;
+        }
+
+        try {
+            return $this->productService->product(
+                (string)$ratingModel->getFieldData('oxobjectid')
+            );
+        } catch (ProductNotFound | InvalidLogin $e) {
+        }
+        return null;
+    }
+}
