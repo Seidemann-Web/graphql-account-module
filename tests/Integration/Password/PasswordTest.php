fa72de1726da28aa82077474efc9b1c3bd2e60d1
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-06-18 13:52:52 +0200

    OXDEV-3565 Add tests for Password controller

---
+++ tests/Integration/Password/PasswordTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\Account\Tests\Integration\Password\Controller;
+
+use OxidEsales\GraphQL\Catalogue\Tests\Integration\TokenTestCase;
+
+final class PasswordTest extends TokenTestCase
+{
+    public function testChangePasswordWithoutToken(): void
+    {
+        $result = $this->query('
+            mutation {
+                userPaswordChange(old: "foobar", new: "foobaz")
+            }
+        ');
+        $this->assertResponseStatus(400, $result);
+    }
+
+    public function testChangePasswordWithWrongOldPassword(): void
+    {
+        $this->prepareToken('admin', 'admin');
+
+        $result = $this->query('
+            mutation {
+                userPaswordChange(old: "foobar", new: "foobaz")
+            }
+        ');
+        $this->assertResponseStatus(403, $result);
+    }
+
+    public function testChangePassword(): void
+    {
+        $this->prepareToken('admin', 'admin');
+
+        $result = $this->query('
+            mutation {
+                userPaswordChange(old: "admin", new: "foobar")
+            }
+        ');
+
+        $this->assertResponseStatus(200, $result);
+
+        $result = $this->query('
+            mutation {
+                userPaswordChange(old: "foobar", new: "admin")
+            }
+        ');
+
+        $this->assertResponseStatus(200, $result);
+    }
+}
