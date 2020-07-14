de025bb724751a76d306659fb972b5c8cb33ab34
hkreuter <heike@reuter-consulting.org>
2020-07-14 15:37:24 +0200

    OXDEV-3707 Add Logged notation

--- src/Account/Controller/DeliveryAddress.php
+++ src/Account/Controller/DeliveryAddress.php
@@ -11,11 +11,13 @@ namespace OxidEsales\GraphQL\Account\Account\Controller;
 
 use OxidEsales\GraphQL\Account\Account\DataType\DeliveryAddress as DeliveryAddressDataType;
 use TheCodingMachine\GraphQLite\Annotations\Mutation;
+use TheCodingMachine\GraphQLite\Annotations\Logged;
 
 final class DeliveryAddress
 {
     /**
      * @Mutation()
+     * @Logged
      */
     public function deliveryAddressAdd(DeliveryAddressDataType $deliveryAddress): DeliveryAddressDataType
     {
