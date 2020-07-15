7bbfda2d340d2afba4b193bfc6a52fb2df5b3f26
hkreuter <heike@reuter-consulting.org>
2020-07-15 12:56:48 +0200

    OXDEV-3707 Extend integration test

--- tests/Integration/Account/Controller/DeliveryAddressTest.php
+++ tests/Integration/Account/Controller/DeliveryAddressTest.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Account\Tests\Integration\Account\Controller;
 
+use OxidEsales\Eshop\Core\Registry as EshopRegistry;
 use OxidEsales\GraphQL\Base\Tests\Integration\TokenTestCase;
 
 final class DeliveryAddressTest extends TokenTestCase
@@ -47,7 +48,7 @@ final class DeliveryAddressTest extends TokenTestCase
         $this->assertResponseStatus(400, $result);
     }
 
-    public function testAddDeliveryAddressForLoggedInUserMissingInput(): void
+    public function testAddDeliveryAddressForLoggedInUserAllInputSet(): void
     {
         $this->prepareToken(self::DIFFERENT_USERNAME, self::DIFFERENT_PASSWORD);
 
@@ -78,6 +79,7 @@ final class DeliveryAddressTest extends TokenTestCase
                 $queryPart .
                 '})
                 {
+                    id
                     salutation
                     firstname
                     lastname
@@ -89,6 +91,9 @@ final class DeliveryAddressTest extends TokenTestCase
                     city
                     phone
                     fax
+                    country {
+                        id
+                    }
                 }
             }'
         );
@@ -97,8 +102,110 @@ final class DeliveryAddressTest extends TokenTestCase
 
         $deliveryAddress = $result['body']['data']['deliveryAddressAdd'];
 
+        $countryId = $inputFields['countryId'];
+        unset($inputFields['countryId']);
+
         foreach ($inputFields as $key => $value) {
             $this->assertSame($value, $deliveryAddress[$key]);
         }
+        $this->assertSame($countryId, $deliveryAddress['country']['id']);
+        $this->assertNotEmpty($deliveryAddress['id']);
+    }
+
+    public function providerRequiredFields()
+    {
+        return [
+            'set1' => [
+                'fields' => [
+                    'oxaddress__oxfname',
+                    'oxaddress__oxlname',
+                    'oxaddress__oxstreet',
+                    'oxaddress__oxstreetnr',
+                    'oxaddress__oxzip',
+                    'oxaddress__oxcity',
+                    'oxaddress__oxcountryid',
+                ],
+            ],
+            'set2' => [
+                'fields' => [
+                    'oxaddress__oxfname',
+                    'oxaddress__oxlname',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerRequiredFields
+     */
+    public function testAddDeliveryAddressForLoggedInUserMissingInput(array $mustFillFields): void
+    {
+        EshopRegistry::getConfig()->setConfigParam('aMustFillFields', $mustFillFields);
+        $prefix = 'Delivery address is missing required fields: ';
+
+        $this->prepareToken(self::DIFFERENT_USERNAME, self::DIFFERENT_PASSWORD);
+
+        $result = $this->query(
+            'mutation {
+                deliveryAddressAdd(deliveryAddress: {' .
+            '})
+                {
+                    salutation
+                }
+            }'
+        );
+
+        $expected = [];
+
+        foreach ($mustFillFields as $field) {
+            $tmp             = explode('__', $field);
+            $name            = ltrim($tmp[1], 'ox');
+            $expected[$name] = $name;
+        }
+        $expected = $prefix . rtrim(implode(', ', $expected), ', ');
+
+        $this->assertResponseStatus(400, $result);
+        $this->assertSame($expected, $result['body']['errors'][0]['message']);
+    }
+
+    public function testAddDeliveryAddressForLoggedInUserInvalidInput(): void
+    {
+        $this->markTestIncomplete('Shop is not validating the input so we mark test as incomplete until further notice.');
+
+        $this->prepareToken(self::DIFFERENT_USERNAME, self::DIFFERENT_PASSWORD);
+
+        $inputFields =  [
+            'salutation'     => 'dual',
+            'firstname'      => str_pad('?ö', 1000, '@'),
+            'lastname'       => 'öäöääöä',
+            'company'        => '1234',
+            'additionalInfo' => str_pad('x', 1000, 'y'),
+            'street'         => str_pad('x', 1000, 'z'),
+            'streetNumber'   => 'is no numbeer',
+            'zipCode'        => 'is no zip',
+            'city'           => 'Freiburg is nice',
+            'countryId'      => 'lalaland',
+            'phone'          => 'fon',
+            'fax'            => 'fax',
+        ];
+
+        $queryPart = '';
+
+        foreach ($inputFields as $key => $value) {
+            $queryPart .= $key . ': "' . $value . '",' . PHP_EOL;
+        }
+
+        $result = $this->query(
+            'mutation {
+                deliveryAddressAdd(deliveryAddress: {' .
+            $queryPart .
+            '})
+                {
+                    firstname
+                }
+            }'
+        );
+
+        $this->assertResponseStatus(400, $result);
     }
 }
