6e38ac7fe2ab5fb43c44923e4fc34a75c8b42e06
hkreuter <heike@reuter-consulting.org>
2020-07-16 13:54:56 +0200

    OXDEV-3706 Extend test

--- tests/Integration/Account/Controller/AddressMultiShopTest.php
+++ tests/Integration/Account/Controller/AddressMultiShopTest.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Account\Tests\Integration\Customer\Controller;
 
+use OxidEsales\Eshop\Application\Model\User as EshopUser;
 use OxidEsales\Eshop\Core\Registry as EshopRegistry;
 use OxidEsales\GraphQL\Base\Tests\Integration\MultishopTestCase;
 
@@ -17,6 +18,12 @@ final class AddressMultiShopTest extends MultishopTestCase
     private const USERNAME = 'user@oxid-esales.com';
 
     private const PASSWORD = 'useruser';
+
+    private const OTHER_USERNAME = 'otheruser@oxid-esales.com';
+
+    private const OTHER_USER_PASSWORD = 'useruser';
+
+    private const OTHER_USER_OXID = '245ad3b5380202966df6ff128e9eecaq';
 
     public function deliveryAddressesDataProviderPerShop()
     {
@@ -130,5 +137,59 @@ final class AddressMultiShopTest extends MultishopTestCase
             'mobile'         => '12345678',
             'fax'            => '555',
         ], $result['body']['data']['customerInvoiceAddressSet']);
+    }
+
+    public function testInvoiceAddressForMallUserFromOtherSubshop(): void
+    {
+        EshopRegistry::getConfig()->setConfigParam('blMallUsers', true);
+        $this->assignUserToShop(1);
+
+        EshopRegistry::getConfig()->setShopId(2);
+        $this->setGETRequestParameter('shp', '2');
+
+        $this->prepareToken(self::OTHER_USERNAME, self::OTHER_USER_PASSWORD);
+
+        $result = $this->query('mutation {
+            customerInvoiceAddressSet(invoiceAddress: {
+                salutation: "MRS"
+                firstname: "Janice"
+                lastname: "Dodo"
+                company: "No GmbH"
+                additionalInfo: "Invoice address"
+                street: "SomeStreet"
+                streetNumber: "999"
+                zipCode: "10000"
+                city: "Any City"
+                countryId: "a7c40f631fc920687.20179984"
+                phone: "123456"
+                mobile: "12345678"
+                fax: "555"
+            }){
+                firstname
+                lastname
+              }
+        }');
+
+        $this->assertResponseStatus(200, $result);
+
+        $this->assertSame(
+            [
+                'firstname' => 'Janice',
+                'lastname'  => 'Dodo',
+            ],
+            $result['body']['data']['customerInvoiceAddressSet']
+        );
+    }
+
+    private function assignUserToShop(int $shopid): void
+    {
+        $user = oxNew(EshopUser::class);
+        $user->load(self::OTHER_USER_OXID);
+        $user->assign(
+            [
+                'oxshopid' => $shopid,
+            ]
+        );
+        $user->save();
     }
 }
