7a8e73ecf5b7e102118498a3c8ed7937fcf86399
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2020-07-29 18:10:31 +0300

    OXDEV-3660 OXDEV-3735 Test basket owner relation

---
+++ tests/Integration/Basket/Controller/BasketOwnerRelationTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\Account\Tests\Integration\Basket\Controller;
+
+use OxidEsales\Eshop\Application\Model\User as EshopUser;
+use OxidEsales\GraphQL\Base\Tests\Integration\TokenTestCase;
+
+final class BasketOwnerRelationTest extends TokenTestCase
+{
+    private const USERNAME = 'user@oxid-esales.com';
+
+    private const USER_ID = 'e7af1c3b786fd02906ccd75698f4e6b9';
+
+    private const OTHER_USERNAME = 'otheruser@oxid-esales.com';
+
+    private const PASSWORD = 'useruser';
+
+    public function testGetPublicBasketWhichOwnerDoesNotExist(): void
+    {
+        $basketId = $this->createPublicBasket();
+        $this->deleteUser(self::USER_ID);
+
+        $this->prepareToken(self::OTHER_USERNAME, self::PASSWORD);
+        $result = $this->query(
+            'query {
+                basket(id: "' . $basketId . '") {
+                    id
+                    owner {
+                        firstName
+                    }
+                }
+            }'
+        );
+
+        $this->assertResponseStatus(404, $result);
+    }
+
+    private function createPublicBasket(): string
+    {
+        $this->prepareToken(self::USERNAME, self::PASSWORD);
+
+        $result = $this->query('mutation {
+            basketCreate(basket: {title: "new-basket-list", public: true}) {
+                id
+            }
+        }');
+
+        $this->setAuthToken('');
+
+        return $result['body']['data']['basketCreate']['id'];
+    }
+
+    private function deleteUser(string $userId): void
+    {
+        $user = oxNew(EshopUser::class);
+        $user->load($userId);
+        $user->delete();
+    }
+}
