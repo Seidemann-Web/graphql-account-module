a8b70cfedea945afed1b84db95b6307bf9fd3986
Anton Fedurtsya <anton@fedurtsya.com>
2020-08-31 18:15:23 +0300

    OXDEV-3820 Add getItems field for Order data type

--- src/Account/DataType/Order.php
+++ src/Account/DataType/Order.php
@@ -123,6 +123,23 @@ final class Order implements DataType
         );
     }
 
+    /**
+     * @Field
+     *
+     * @return OrderItem[]
+     */
+    public function getItems(): array
+    {
+        $items = [];
+        $orderArticles = $this->order->getOrderArticles();
+
+        foreach ($orderArticles as $oneArticle) {
+            $items[] = new OrderItem($oneArticle);
+        }
+
+        return $items;
+    }
+
     public static function getModelClass(): string
     {
         return EshopOrderModel::class;
