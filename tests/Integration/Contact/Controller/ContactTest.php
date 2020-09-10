4f65a079e57929134ad56d320cfca44bddbd23fc
Anton Fedurtsya <anton@fedurtsya.com>
2020-09-10 17:43:02 +0300

    OXDEV-3960 Test Contact request flow

---
+++ tests/Integration/Contact/Controller/ContactTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\Account\Tests\Integration\Contact\Controller;
+
+use OxidEsales\GraphQL\Base\Tests\Integration\TokenTestCase;
+
+final class ContactTest extends TokenTestCase
+{
+    public function testContactRequest(): void
+    {
+        $result = $this->query(
+            'mutation{contactRequest(contactRequest:{
+              email:"sometest@somedomain.com"
+              firstName:"myName"
+              lastName:"mySurname"
+              salutation:"mr"
+              subject:"some subject"
+              message:"some message"
+            })}'
+        );
+
+        $this->assertResponseStatus(200, $result);
+
+        $this->assertTrue($result['body']['data']['contactRequest']);
+    }
+
+    public function testContactRequestUsesShopValidation(): void
+    {
+        $result = $this->query(
+            'mutation{contactRequest(contactRequest:{
+              email:"wrongEmail"
+            })}'
+        );
+
+        $this->assertResponseStatus(400, $result);
+        $error = $result['body']['errors'][0];
+
+        $this->assertStringStartsWith("Contact request validation failed with error:", $error['message']);
+    }
+}
